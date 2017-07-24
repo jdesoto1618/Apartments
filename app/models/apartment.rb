ee9ec410e6a7cba56d8cb5826713b8379000088f
Joe <modulusman@gmail.com>
2017-07-24 14:11:29 -0700

    Added validation to state field

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -2,4 +2,5 @@ class Apartment < ApplicationRecord
   #Place the validates here so that fields can't be empty. This is server side?
   validates :street, :city, :zipcode, :state, :country, :name, :phone, :hours, presence: true
   validates :zipcode, length: { is: 5 }
+  validates :state, length: { is: 2 }
 end
