e2e1d3bca5e06ce925ce23413a1a974d62807896
rithy <oukrithy@yahoo.com>
2017-07-24 13:55:42 -0700

    add zipcode validation

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,5 @@
 class Apartment < ApplicationRecord
   #Place the validates here so that fields can't be empty. This is server side?
   validates :street, :city, :zipcode, :state, :country, :name, :phone, :hours, presence: true
+  validates :zipcode, length: { is: 5 }
 end
